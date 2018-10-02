7fe9bc23b44f7cbc219c84c767d84c8f2b00e908
mkadirtan <31560865+mkadirtan@users.noreply.github.com>
2018-10-02 17:11:17 +0300

    Add files via upload
    wtf.cpp 210 dersinde not tutuyorum. Ona takılmayın

--- src/my_meshes.js
+++ src/my_meshes.js
@@ -1,76 +1,134 @@
 import * as BABYLON from 'babylonjs';
+import {TweenLite, TimelineMax} from 'gsap/TweenMax';
 
-//this.mesh.name için bi bakmak lazım.
 export default function CreateAnimatableMesh(MeshCreator){
 	this.mesh = new MeshCreator;
-	Object.defineProperty(this, "timeline_data", {
+	Object.defineProperty(this, "data", {
 		value: {
 			"name": this.mesh.name,
-			"loc": [],
+			"pos": [],
 			"rot": [],
-			"timeline": undefined,
-			"animations": []
+			//"anim": [],
+			//"ranges": this.mesh.skeleton.getAnimationRanges(),
+			//"currentFrame": 0,
+			"animationGroup": new BABYLON.AnimationGroup(this.mesh.name),
+			"timeline": new TimelineMax()
+		},
+		enumerable: true,
+		configurable: true,
+		writeable: true
+	});
+
+	//Add all the animations to animationGroup.
+	//Animation differentiations are based on ranges
+	//Ex: walk animation is in range of frames 0-9
+
+	/*for(let bone of this.mesh.skeleton.bones){
+		let anims = bone.animations;
+		for(let anim of anims){
+			this.data.animationGroup.addTargetedAnimation(anim, bone)
 		}
-		enumerable: true;
-		configurable: true;
-		writeable: true;
-	})
+	};*/
 };
 
-CreateAnimatableMesh.prototype.addKey = function(aniObj){
-	/*aniObj = {
-		"T": time,
-		"loc": loc,
-		"rot": rot,
-		"animateLoc": true,
-		"animateRot": false
-		"characterAnimation": {
-			"type": "walkLikeMan",
-			"repeat": stepCount,
-			"timeline": 
-		}
-		"slider": slider,
-		"sensitivity": sensitivity
-	}*/
+CreateAnimatableMesh.prototype = {
+	addKey: function(aniObj){
 
-	let isDuplicate = false;
+		//animateSth is a boolean to whether or not add key frame for Sth.
 
-	if(aniObj.animateLoc){
+		if(aniObj.animatePos){
+			pushKeys(aniObj.positionPush, this.data.position, "pos")
+		};
 		
-		//pos objects property, loc recorded property
-		let pos = aniObj.loc;
-	    let loc = thisinstance?.timeline_data.loc;
+		if(aniObj.animateRot){
+			pushKeys(aniObj.rotationPush, this.data.rotation, "rot");
+		};
 
-	    loc.forEach(
-	    	function(entry){
-	    		if(Math.abs(entry.T - slider.value) <= sensitivity){
-	            loc[loc.indexOf(entry)] = {"T": aniObj.T, "position": pos};
-	            isDuplicate = true;
-	            console.log("Notification: A keyframe duplicate was found, replacing old keyframe entry!")}});
+		//Interchangable function to add new keyframes to "current".
+		//push is an object with T and the values to be added.
+		//type decides which data of timeline is to be updated.
 
-	    if(!isDuplicate){
-	        loc.push({"T": aniObj.T, "position": pos})}
-	    updateTL(thisinstance?.timeline_data.timeline, "loc")}
-	
-	if(aniObj.animateRot){
-		isDuplicate = false;
+		function pushKeys(push, current, type){
+			let isDuplicate = false;
+			current.forEach(
+				function(entry, index, array){
+					if(Math.abs(entry.T - aniObj.T) <= aniObj.sensitivity){
+						array[index] = push;
+						isDuplicate = true;
+						console.log("Notification: A keyframe duplicate was found, replacing old keyframe entry!")
+					};
+				}
+			);
 
-		//turn objects property, rot recorded property
-		let turn = aniObj.rot;
-	    let rot = thisinstance?.timeline_data.rot;
+			if(!isDuplicate){
+				current.push(push)
+			};
+			updateTimeline(type);
+		};
+	},
+	//Updates the timeline of selected data type.
+	updateTimeline: function(type){
+		//current -> keyframe data of the object
+		//property -> property to be updated in the timeline
+		//killObject -> data structure to be erased
+		let data = this.data;
+		let current;
+		let property;
+		let killObject;
 
-	    rot.forEach(
-	    	function(entry){
-	    		if(Math.abs(entry.T - slider.value) <= sensitivity){
-	            rot[rot.indexOf(entry)] = {"T": aniObj.T, "rotation": rot};
-	            isDuplicate = true;
-	        	console.log("Notification: A keyframe duplicate was found, replacing old keyframe entry!")}});
+		switch(type){
+			case "pos":
+			current = this.data.pos;
+			property = this.mesh.position;
+			killObject = {x: true, y:true, z: true};
+			break;
+			case "rot":
+			current = this.data.rot;
+			property = this.mesh.rotation;
+			killObject = {x: true, y: true, z: true};
+			break;
+		};
 
-	    if(!isDuplicate){rot.push({"T": aniObj.T, "rotation": rot})}
-	    updateTL(thisinstance?.timeline_data.timeline, "rot")}
-}
+		//Sort current keys
 
-export default function CreateNonAnimatableMesh(MeshCreator){
+		if(current.length > 1){
+			current.sort(function(a,b){
+				return a.T- b.T;
+			});
+		};
+
+		//Erase previous timeline data
+
+		this.data.timeline.kill(killObject, property);
+
+		//Add new keys
+
+		current.forEach(function(entry, index, array){
+			//Starting from index=1
+			if(index !== 0){
+				//Duration of every tween is the time difference between subsequent keys.
+				let deltaTime = entry.T-array[index-1].T;
+				//entry = array[index], therefore index-1 obtains element just before;
+				tl.fromTo(property, deltaT, array[index-1].value, entry.value, array[index-1].T);
+				let animation = new TimelineMax().fromTo()
+			}
+		});
+
+    	tl.seek(tl.totalDuration()/2).seek(slider.value-0.001);
+	},
+	getAniObj: function(){
+		return {
+			"T": slider.T,
+			"positionPush": {"T": slider.T, "value": this.mesh.position},
+			"rotationPush": {"T": slider.T, "value": this.mesh.rotation},
+			"animatePosition": animationSettings.animatePosition,
+			"animateRotation": animationSettings.animateRotation,
+			"sensitivity": animationSettings.sensitivity
+		}
+	}
+};
+
+export function CreateNonAnimatableMesh(MeshCreator){
 	let result = new MeshCreator;
 	result.isPickable = false;
 	return result;
