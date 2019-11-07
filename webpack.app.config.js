962543e9fe71c3a6812ab2ece673698f69846d14
Muhammed <muhammedkadirtan@gmail.com>
2019-11-07 11:38:10 +0300

    Removed unused dependencies

--- webpack.app.config.js
+++ webpack.app.config.js
@@ -1,36 +1,18 @@
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     context: __dirname,
     entry: __dirname + '/app/index.js',
     output: {
-        filename: 'app.js',
+        filename: 'index.js',
         path: __dirname + '/dist'},
     plugins: [
         new webpack.ProgressPlugin(),
-        new webpack.ProvidePlugin({'earcut': 'earcut'}),
-        new HtmlWebpackPlugin({
-            'template': __dirname + '/app/index.html',
-            'filename': __dirname + '/dist/app.html',
-            'favicon': __dirname + '/shared/textures/favicon.ico'})],
+        new webpack.ProvidePlugin({'earcut': 'earcut'})
+        ],
     module: {
         rules: [
-            {
-                test: /\.(png)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '/image/app/[name].[ext]'
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                    }
-                ]
-            },
             {
                 test: /\.m?js$/,
                 exclude: /(node_modules|bower_components)/,
